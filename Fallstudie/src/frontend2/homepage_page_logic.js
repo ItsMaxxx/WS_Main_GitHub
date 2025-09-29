31bc027a801c1a81fece0fede5272153d238015f
ItsMaxxx <Schmelzer2040@gmail.com>
2025-09-30 01:55:12 +0200

    Von Grund auf neues Design
    Alte Website Scrapped und neue erstellt

--- Fallstudie/src/frontend2/homepage_page_logic.js
+++
@@ -1,9 +0,0 @@
-
-/* Logik für das Umschalten zwischen den Pages*/
-function showPage(page) {
-    const pages = document.querySelectorAll('.login-page, .register-page');
-    pages.forEach(p => p.classList.add('hidden'));
-    if (page === 'login') document.querySelector('.login-page').classList.remove('hidden');
-    if (page === 'register') document.querySelector('.register-page').classList.remove('hidden');
-}
-document.addEventListener('DOMContentLoaded', () => showPage('login'));
